c2fc85a510602e694e0d21c70c5be3a91dc31985
leejungwu <ljw538@naver.com>
2020-09-11 17:54:50 +0900

    master

--- 틱택토.js
+++ 틱택토.js
@@ -5,11 +5,67 @@ var 칸들 = [];
 var 턴 = 'X';
 var 결과 = document.createElement('div');
 
-var 비동기콜백 = function(e){    
+function 결과체크(몇줄, 몇칸) {
+    // 세칸 다 채워졌나?
+    var 다참 = false;
+    // 가로줄 검사
+    if (
+        칸들[몇줄][0].textContent === 턴 &&
+        칸들[몇줄][1].textContent === 턴 && 
+        칸들[몇줄][2].textContent === 턴
+    ) {
+        다참 = true;
+    }
+    // 세로줄 검사
+    if (
+        칸들[0][몇칸].textContent === 턴 && 
+        칸들[1][몇칸].textContent === 턴 && 
+        칸들[2][몇칸].textContent === 턴
+    ) {
+        다참 = true;
+    }
+    
+    // 대각선 검사 필요한 경우
+    if (칸들[0][0].textContent === 턴 &&
+        칸들[1][1].textContent === 턴 &&
+        칸들[2][2].textContent === 턴
+        ) {
+            다참 = true;
+        }
+     
+    if (칸들[0][2].textContent === 턴 &&
+            칸들[1][1].textContent === 턴 &&
+            칸들[2][0].textContent === 턴
+            ) {
+            다참 = true;
+    } 
+    return 다참;
+}
+
+function 초기화(무승부) {
+    if (무승부) {
+        결과.textContent = '무승부';
+    } else {
+        결과.textContent = 턴 + '님이 승리!';
+    }
+    
+    setTimeout(function(){
+        결과.textContent = '';
+        칸들.forEach(function (줄) {
+            줄.forEach(function (칸) {
+                칸.textContent = '';
+            });
+        });
+        턴 = 'X';
+    },1000);       
+}
+
+var 비동기콜백 = function(e){  //칸을 클릭했을 때  
+    if (턴 == 'O') {
+        return;
+    }
     var 몇줄 = 줄들.indexOf(e.target.parentNode);
-    console.log('몇줄', 몇줄);
     var 몇칸 = 칸들[몇줄].indexOf(e.target);
-    console.log('몇칸', 몇칸);
 
     if (칸들[몇줄][몇칸].textContent !== "") { //칸이 이미 채워져 있는가?
         console.log('빈칸이 아닙니다.');    
@@ -17,50 +73,20 @@ var 비동기콜백 = function(e){
         console.log('빈칸 입니다');
         칸들[몇줄][몇칸].textContent = 턴;
 
-         // 세칸 다 채워졌나?
-        var 다참 = false;
-        // 가로줄 검사
-        if (
-            칸들[몇줄][0].textContent === 턴 &&
-            칸들[몇줄][1].textContent === 턴 && 
-            칸들[몇줄][2].textContent === 턴
-        ) {
-            다참 = true;
-        }
-        // 세로줄 검사
-        if (
-            칸들[0][몇칸].textContent === 턴 && 
-            칸들[1][몇칸].textContent === 턴 && 
-            칸들[2][몇칸].textContent === 턴
-        ) {
-            다참 = true;
-        }
-        
-        // 대각선 검사 필요한 경우
-        if (칸들[0][0].textContent === 턴 &&
-            칸들[1][1].textContent === 턴 &&
-            칸들[2][2].textContent === 턴
-            ) {
-                다참 = true;
-            }
-         
-        if (칸들[0][2].textContent === 턴 &&
-                칸들[1][1].textContent === 턴 &&
-                칸들[2][0].textContent === 턴
-                ) {
-                다참 = true;
-        }
-        
+        var 다참 = 결과체크(몇줄, 몇칸);
+        // 모든 칸이 다 찼는지 검사
+        var 후보칸 = [];
+        칸들.forEach(function (줄) {
+            줄.forEach(function (칸) {
+                후보칸.push(칸);
+            });
+        });
+        후보칸 = 후보칸.filter(function (칸) { return !칸.textContent }); 
         // 다 찼으면
         if (다참) {
-            결과.textContent = 턴 + '님이 승리!';
-            //초기화
-            턴 = 'X';
-            칸들.forEach(function (줄) {
-                줄.forEach(function (칸) {
-                    칸.textContent = '';
-                })
-            })
+            초기화();
+        } else if (후보칸.length === 0) { // 칸을 더이상 선택할 수 없음
+            초기화(true);
         } else {
             if (턴 === 'X'){
                 턴 = 'O';
@@ -68,60 +94,17 @@ var 비동기콜백 = function(e){
             setTimeout(function() {
                 console.log('컴퓨터의 턴입니다.');
                 // 빈 칸 중 하나를 고른다.
-                var 후보간 = [];
-                칸들.forEach(function (줄) {
-                    줄.forEach(function (칸) {
-                        후보칸.push(칸);
-                    });
-                });
-                후보칸 = 후보칸.filter(function (칸) { return !칸.textContent });   
+  
                 var 선택칸 = 후보칸[Math.floor(Math.random() * 후보칸.length)];   
                 선택칸.textContent = 턴;
                 // 컴퓨터가 승리했는지 체크
-                // 세칸 다 채워졌나?
-                var 다참 = false;
-                // 가로줄 검사
-                if (
-                    칸들[몇줄][0].textContent === 턴 &&
-                    칸들[몇줄][1].textContent === 턴 && 
-                    칸들[몇줄][2].textContent === 턴
-                ) {
-                    다참 = true;
-                }
-                // 세로줄 검사
-                if (
-                    칸들[0][몇칸].textContent === 턴 && 
-                    칸들[1][몇칸].textContent === 턴 && 
-                    칸들[2][몇칸].textContent === 턴
-                ) {
-                    다참 = true;
-                }
-                
-                // 대각선 검사 필요한 경우
-                if (칸들[0][0].textContent === 턴 &&
-                    칸들[1][1].textContent === 턴 &&
-                    칸들[2][2].textContent === 턴
-                    ) {
-                        다참 = true;
-                    }
-                
-                if (칸들[0][2].textContent === 턴 &&
-                        칸들[1][1].textContent === 턴 &&
-                        칸들[2][0].textContent === 턴
-                        ) {
-                        다참 = true;
-                }
+                var 몇줄 = 줄들.indexOf(선택칸.parentNode);
+                var 몇칸 = 칸들[몇줄].indexOf(선택칸);  
+                var 다참 = 결과체크(몇줄, 몇칸);
                 
                 // 다 찼으면
                 if (다참) {
-                    결과.textContent = 턴 + '님이 승리!';
-                    //초기화
-                    턴 = 'X';
-                    칸들.forEach(function (줄) {
-                        줄.forEach(function (칸) {
-                            칸.textContent = '';
-                        });
-                    });
+                    초기화();
                 }
                 // 턴을 나한테 넘긴다.
                 턴 = 'X';
@@ -130,7 +113,7 @@ var 비동기콜백 = function(e){
     }
 };
 
-for(var i = 1; i<=3; i+=1){
+for(var i = 1; i<=3; i+=1){ 
     var 줄 = document.createElement('tr');
     줄들.push(줄);
     칸들.push([]);
@@ -145,4 +128,3 @@ for(var i = 1; i<=3; i+=1){
 
 바디.append(테이블);
 바디.append(결과);
-console.log(줄들,칸들);
